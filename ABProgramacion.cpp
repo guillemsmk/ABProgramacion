3a2e8b5e0cb4b2fb45b80643e854b0e6515e6303
guillemsmk <157405270+guillemsmk@users.noreply.github.com>
2024-11-26 16:47:35 +0100

    Agregar archivos de proyecto.

---
+++ ABProgramacion.cpp
@@ -0,0 +1,12 @@
+﻿// ABProgramacion.cpp: define el punto de entrada de la aplicación.
+//
+
+#include "ABProgramacion.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
